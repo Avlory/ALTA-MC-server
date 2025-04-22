cb9350c91464b6f52c5bda28dbb91a98383cbe21
Sean D Ward <119538683+Sean-Ward-The-Albino@users.noreply.github.com>
2025-04-22 10:06:33 -0700

    Spring Boot Back-End

---
+++ altamcservwer/altamcservwer/src/main/java/com/avlory/altamcservwer/AltamcservwerApplication.java
@@ -0,0 +1,13 @@
+package com.avlory.altamcservwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltamcservwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltamcservwerApplication.class, args);
+	}
+
+}
---
+++ altamcservwer/altamcservwer/src/test/java/com/avlory/altamcservwer/AltamcservwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avlory.altamcservwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltamcservwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
